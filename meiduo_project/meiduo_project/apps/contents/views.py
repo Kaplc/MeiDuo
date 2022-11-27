95bab705d520a1e990a3246dc9350a24ec67b53d
Kapl <755536175@qq.com>
2022-11-27 22:10:09 +0800

    查询首页商品广告

--- meiduo_project/meiduo_project/apps/contents/views.py
+++ meiduo_project/meiduo_project/apps/contents/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 from collections import OrderedDict
-from goods.models import GoodsChannelGroup, GoodsChannel, GoodsCategory
 from goods.utils import get_categories
+from .models import ContentCategory
 
 class RedirectIndex(View):
     """重定向到首页"""
@@ -19,9 +19,14 @@ class IndexView(View):
         """提供首页广告"""
         # 查询商品频道和分类
         categories = get_categories()
-
+        # 查询首页广告数据
+        contents = OrderedDict()
+        for cat in ContentCategory.objects.all():
+            contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
+        # 定义上下文
         context = {
-            'categories': categories
+            'categories': categories,
+            'contents': contents,
         }
 
         return render(request, 'index.html', context)
