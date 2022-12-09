ac205223750fd5068c9bca9d74fa9cf3b7a07653
Kapl <755536175@qq.com>
2022-12-09 19:09:43 +0800

    创建管理订单子应用

--- meiduo_project/meiduo_project/static/js/list.js
+++ meiduo_project/meiduo_project/static/js/list.js
@@ -53,5 +53,31 @@ let vm = new Vue({
                     console.log(error.response);
                 })
         },
+        // 加入购物车
+        add_carts(sku_id) {
+            let url = '/carts/';
+            axios.post(url, {
+                sku_id: sku_id,
+                count: 1
+            }, {
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken')
+                },
+                responseType: 'json',
+                withCredentials: true
+            })
+                .then(response => {
+                    if (response.data.code == '0') {
+                        this.get_carts()
+                        alert('添加购物车成功');
+
+                    } else { // 参数错误
+                        alert(response.data.errmsg);
+                    }
+                })
+                .catch(error => {
+                    console.log(error.response);
+                })
+        },
     }
 });
--- meiduo_project/meiduo_project/static/js/search.js
+++ meiduo_project/meiduo_project/static/js/search.js
@@ -3,9 +3,36 @@ let vm = new Vue({
     delimiters: ['[[', ']]'],
     data: {
         username: getCookie('username'),
+
     },
-    mounted(){
+    mounted() {
+
     },
     methods: {
+        // 加入购物车
+        add_carts(sku_id) {
+            let url = '/carts/';
+            axios.post(url, {
+                sku_id: sku_id,
+                count: 1
+            }, {
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken')
+                },
+                responseType: 'json',
+                withCredentials: true
+            })
+                .then(response => {
+                    if (response.data.code == '0') {
+                        alert('添加购物车成功');
+
+                    } else { // 参数错误
+                        alert(response.data.errmsg);
+                    }
+                })
+                .catch(error => {
+                    console.log(error.response);
+                })
+        },
     }
 });
