2628e647d41129ca52cbb13c58ecc16c330a1560
Kaplc <2413528148@qq.com>
2023-03-31 22:21:44 +0800

    获取订单详细信息

--- meiduo_project/meiduo_project/apps/meiduo_admin/serializers/orders_serializer.py
+++ meiduo_project/meiduo_project/apps/meiduo_admin/serializers/orders_serializer.py
@@ -1,15 +1,35 @@
 from rest_framework import serializers
-from orders.models import OrderInfo
+
+from goods.models import SKU
+from orders.models import OrderInfo, OrderGoods
+
+
+class SKUSerializer(serializers.ModelSerializer):
+    """sku"""
+
+    class Meta:
+        model = SKU
+        fields = ('name', 'default_image_url')
+
+
+class OrderGoodsSerializer(serializers.ModelSerializer):
+    """OrderGoods"""
+    sku = SKUSerializer()
+
+    class Meta:
+        model = OrderGoods
+        fields = ('count', 'price', 'sku')
 
 
 class OrdersSerializer(serializers.ModelSerializer):
     """订单serializer"""
     user = serializers.StringRelatedField()
+    skus = OrderGoodsSerializer(many=True)
 
     class Meta:
         model = OrderInfo
         fields = ('order_id', 'user', 'total_count', 'total_amount', 'freight',
-                  'pay_method', 'status', 'create_time',)
+                  'pay_method', 'status', 'create_time', 'skus')
 
 
 """
